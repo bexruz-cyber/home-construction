5bf9893c4a21ffddd6ba0014e4d16a1e264a9641
bexruz-cyber <mayoq2024@gmail.com>
2025-01-13 16:49:43 +0500

    add hero swiper : Grafeas

--- js/app.js
+++ js/app.js
@@ -1,25 +1,32 @@
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3, // Bir qatorga 3ta slayd ko'rsatish
-    spaceBetween: 40, // Slaydlar orasidagi masofa
-    navigation: {
-        nextEl: ".swiper-button-next1",
-        prevEl: ".swiper-button-prev1",
-    },
-    pagination: {
-        el: ".swiper-pagination-california",
-        clickable: true,
-    },
-    loop: true,
-    
+  slidesPerView: 3, // Bir qatorga 3ta slayd ko'rsatish
+  spaceBetween: 40, // Slaydlar orasidagi masofa
+  navigation: {
+    nextEl: ".swiper-button-next1",
+    prevEl: ".swiper-button-prev1",
+  },
+  pagination: {
+    el: ".swiper-pagination-california",
+    clickable: true,
+  },
+  loop: true,
 });
 
+var swiperHero = new Swiper(".mySwiperHero", {
+  loop: true,
+  speed: 800,
+  autoplay: {
+    delay: 3000, // 1000 milliseconds = 1 second
+    disableOnInteraction: false, // Autoplay continues after user interaction
+  },
+});
 
-const button = document.getElementById('scrollBtn');
-button.addEventListener('click', (e) => {
-    e.preventDefault(); // Standart harakatni to'xtatadi
-    document.querySelector('#section2').scrollIntoView({
-        behavior: 'smooth'
-    });
+const button = document.getElementById("scrollBtn");
+button.addEventListener("click", (e) => {
+  e.preventDefault(); // Standart harakatni to'xtatadi
+  document.querySelector("#section2").scrollIntoView({
+    behavior: "smooth",
+  });
 });
 
 AOS.init();
